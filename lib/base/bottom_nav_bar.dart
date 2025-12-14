04d0247c7a6f0576930b3dbfa0c572a2dd874a80
csi-demo <ara07shukla@gamil.com>
2025-12-14 14:42:29 +0530

    added bottom navigation bar

---
+++ lib/base/bottom_nav_bar.dart
@@ -0,0 +1,56 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatefulWidget {
+   BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+ final appScreen =[
+  Center(child: Text('HOME')),
+  Center(child: Text('SEARCH')),
+  Center(child: Text('TICKET')),
+  Center(child: Text('PROFILE'))
+ ];
+  // _ means private key which is accessable to class only 
+  //change our index for BottomNavBar
+  int _selectedIndex =0;
+   void _onitemTapped(int index){
+    setState(() {
+      _selectedIndex=index;
+    });
+   }
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('Ticket APP')),
+      ),
+      body: appScreen[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onitemTapped,
+        selectedItemColor: Colors.blueGrey,
+        unselectedItemColor: Color(0xFF9E9E9E),
+        showSelectedLabels: false,
+        items: [
+          BottomNavigationBarItem(icon:Icon(FluentSystemIcons.ic_fluent_home_add_regular),
+          activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
+          label: 'HOME'),
+           BottomNavigationBarItem(icon:Icon(FluentSystemIcons.ic_fluent_search_regular),
+          activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
+          label: 'SEARCH  '),
+           BottomNavigationBarItem(icon:Icon(FluentSystemIcons.ic_fluent_ticket_regular),
+          activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),
+          label: 'TICKET'),
+           BottomNavigationBarItem(icon:Icon(FluentSystemIcons.ic_fluent_person_regular),
+          activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
+          label: 'PROFILE'),
+        ],
+      ),
+    );
+  }
+}
