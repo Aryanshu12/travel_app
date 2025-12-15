46517d45c38cc995ab3cc50866abde8bd7ba650b
csi-demo <ara07shukla@gamil.com>
2025-12-15 19:59:45 +0530

    added style folder for app

---
+++ lib/base/resource/style/app_styles.dart
@@ -0,0 +1,19 @@
+import "package:flutter/material.dart";
+
+Color Primary =  Color(0xFF687daf);
+class AppStyles {
+  static Color primaryColor=Primary;
+  static Color Textcolor = Color(0xFF3b3b3b);
+
+ static TextStyle headlineStyle1=TextStyle(
+  fontSize: 17,
+  fontWeight: FontWeight.w500,
+  color: Textcolor,
+  );
+  static TextStyle headlineStyle2=TextStyle(
+  fontSize: 26,
+  fontWeight: FontWeight.w500,
+  color: Textcolor,
+  );
+
+}
