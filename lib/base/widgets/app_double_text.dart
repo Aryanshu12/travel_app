66b16312e11a7012b76f967a7afd982d4d3d9b64
Aryanshu12 <ara07shukla@gamil.com>
2025-12-18 11:07:28 +0530

    change the text style

---
+++ lib/base/widgets/app_double_text.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app/base/resource/style/app_styles.dart';
+
+class AppDoubleText extends StatelessWidget {
+   AppDoubleText({super.key,required this.bigText,required this.smallText});
+   final String bigText;
+    final String smallText;
+
+  @override
+  Widget build(BuildContext context) {
+    return  Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+     Text(bigText,style: AppStyles.headlineStyle2),
+     InkWell(
+      onTap: (){
+
+      },
+      child: Text(smallText,style: AppStyles.textStyle.copyWith(color: AppStyles.headlineStyle1.color)),
+     ),
+     ],
+     );
+  }
+}
